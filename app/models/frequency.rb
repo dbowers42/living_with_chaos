3b7c5124f51a6dfd19ef3e9466242f8422a94ebb
dbowers42 <dbowers42@gmail.com>
2016-09-12 13:34:51 -0600

    fix repo

--- app/models/frequency.rb
+++ app/models/frequency.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: frequencies
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  days        :integer
+#  how_often   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Frequency < ActiveRecord::Base
 end
