50cfe841196c177a4c8b41ba410d1930406d73b1
dbowers42 <dbowers42@gmail.com>
2016-09-13 13:12:04 -0600

    show random tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,14 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
+
+    ids = Story.where('id > 1') # TODO: add some condition to determine story is available for selection
+        .order('random()')
+        .limit(4)
+        .pluck(:id)
+
+    @stories = Story.find(ids)
+
   end
 
   def add
